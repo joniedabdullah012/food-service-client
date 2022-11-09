f76bfcf7b4fa2309eebc3540b4f168dff661f513
joniedabdullah012 <jaarafathbd360@gmail.com>
2022-11-10 01:55:00 +0600

    header and navbar logo add and degin update

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../../../Assest/logo/logo.svg.jpg'
 
 
 const Header = () => {
     return (
-        <div className="navbar bg-base-100">
-            <div className="flex-1">
+        <div className="navbar h-20 mb-12 bg-base-100">
+            <div className="flex-1 ">
+
+                <img className='w-20' src={logo} alt=''></img>
 
                 <Link to='/' className="btn btn-ghost normal-case text-xl">Food Services</Link>
+
 
             </div>
 
             <div className="flex-none">
                 <ul className="menu menu-horizontal p-0">
                     <li><a>Home</a></li>
-                    <li tabIndex={0}>
+                    <li>
                         <a>
                             Parent
 
